1794f2ccfeb668ab3cda5766d52880a6b3a76a41
SireNET <SireNET@tuta.io>
2025-09-29 22:22:42 -0400

    Added installer script

---
+++ installer.js
@@ -0,0 +1,170 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  // For testing only!
+  ns.ui.openTail();
+
+  const baseURL = "https://raw.githubusercontent.com/aGOLDstandard2/Sire-Botnet/main/";
+  ns.print(`Info: Downloading files...`);
+  ns.tprint(`Info: Downloading files, please wait...`);
+  let a = false;
+  const maxRetry = 3;
+  let f = 1
+  let fileFails = [];
+  let skipCounter = 0;
+  const fileCount = 16;
+
+  // Main Script
+  if (ns.fileExists("/sire.js")) {
+    ns.print(`Info: sire.js already found. Skipping.`);
+    a = true;
+    f++;
+    skipCounter++;
+  }
+  if (!a) {
+    let retryCounter = 0;
+    while (!ns.fileExists("/sire.js") && retryCounter < maxRetry) {
+      ns.print(`Downloading file ${f}/16 - Attempt ${retryCounter + 1}/3`);
+      await ns.wget(`${baseURL}sire.js`, `/sire.js`);
+      retryCounter++;
+
+      if (retryCounter >= maxRetry) {
+        ns.print(`Warn: File extraction failed!`);
+        fileFails.push(`/sire.js`);
+        f++
+      }
+      if (ns.fileExists("/sire.js")) {
+        ns.print(`Success: sire.js downloaded!`);
+        ns.tprint(`Success: file ${f}/16 aqquired!`);
+        f++
+      }
+    }
+  }
+
+  // Logs dir creation
+  if (ns.fileExists(`/logs/testLog.txt`)) {
+    ns.print(`Info: /logs directory already found. Skipping.`);
+    a = true;
+    f++;
+    skipCounter++;
+  }
+  if (!a) {
+    let retryCounter = 0;
+    while (!ns.fileExists("/logs/testLog.txt") && retryCounter < maxRetry) {
+      ns.print(`Downloading file ${f}/16 - Attempt ${retryCounter + 1}/3`);
+      await ns.wget(`${baseURL}logs/testLog.txt`, `/logs/testLog.txt`);
+      retryCounter++;
+
+      if (retryCounter >= maxRetry) {
+        ns.print(`Warn: File extraction failed!`);
+        fileFails.push(`/logs/testLog.txt`);
+        f++;
+      }
+      if (ns.fileExists("/logs/testLog.js")) {
+        ns.print(`Success: /logs dir created!`);
+        ns.tprint(`Success: file ${f}/16 aqquired!`);
+        f++;
+      }
+    }
+  }
+
+  // Tools
+  const tools = [`cleanup.js`, `listener.js`, `jump.js`, `patient.js`, `patient2.js`, `updater.js`];
+  for (let i = 0; i < tools.length; i++) {
+    if (ns.fileExists(`/tools/${tools[i]}`)) {
+      ns.print(`Info: ${tools[i]} already found. Skipping.`);
+      a = true;
+      f++;
+      skipCounter++;
+    }
+    if (!a) {
+      let retryCounter = 0;
+      while (!ns.fileExists(`/tools/${tools[i]}`) && retryCounter < maxRetry) {
+        ns.print(`Downloading file ${f}/16 - Attempt ${retryCounter + 1}/3`);
+        await ns.wget(`${baseURL}tools/${tools[i]}`, `/tools/${tools[i]}`);
+        retryCounter++;
+
+        if (retryCounter >= maxRetry) {
+          ns.print(`Error: File extraction failed!`);
+          fileFails.push(`/tools/${tools[i]}`);
+          f++;
+        }
+        if (ns.fileExists(`/tools/${tools[i]}`)) {
+          ns.print(`Success: ${tools[i]} downloaded!`);
+          ns.tprint(`Success: file ${f}/16 aqquired!`);
+          f++;
+        }
+      }
+    }
+  }
+
+  // Utils
+  const utils = [`alias.txt`, `guestBook.txt`, `lkt.txt`, `serverInfo.txt`, `sireLib.js`, `w.js`, `h.js`, `g.js`];
+  for (let i = 0; i < utils.length; i++) {
+    if (ns.fileExists(`/utils/${utils[i]}`)) {
+      ns.print(`Info: ${utils[i]} already found. Skipping.`);
+      a = true;
+      f++;
+      skipCounter++;
+      ns.print(`${skipCounter}`);
+    }
+    if (!a) {
+      let retryCounter = 0;
+      while (!ns.fileExists(`/utils/${utils[i]}`) && retryCounter < maxRetry) {
+        ns.print(`Downloading file ${f}/16 - Attempt ${retryCounter + 1}/3`);
+        retryCounter++;
+
+        if (retryCounter >= maxRetry) {
+          ns.print(`Error: File extraction failed!`);
+          fileFails.push(`/utils/${utils[i]}`);
+          f++;
+        }
+        if (ns.fileExists(`/utils/${utils[i]}`)) {
+          ns.print(`Success: ${utils[i]} downloaded!`);
+          ns.tprint(`Success: file ${f}/16 aqquired!`);
+          f++;
+        }
+      }
+    }
+  }
+
+  // Cleanup
+  ns.print(`Info: Cleaning up...`);
+  ns.tprint(`Info: Cleaning up...`);
+  if (skipCounter === fileCount) {
+    ns.tprint(`Success: No missing files found! Stopping installer.`);
+  } else if (fileFails.length > 0) {
+    ns.print(`Error: Some files failed to transfer.`);
+    ns.print(`Error: Printing filenames to terminal.`);
+    ns.tprint(`ERROR: The following files failed to transfer!`);
+    for (let i = 0; fileFails.length > 0; i++) {
+      if (i === fileFails.length) {
+        ns.tprint(`WARN: ${fileFails[i]}`);
+      } else {
+        ns.tprint(`WARN: ${fileFails[i]},`)
+      }
+    }
+    ns.tprint(`ERROR: Please check with your network administrator.`);
+    ns.exit();
+  } else {
+    ns.tprint(`Success: All files attained! Stopping installer.`);
+    ns.print(`Info: Sending quickstart UI prompt`);
+    await ns.sleep(300);
+    const helpCheck = await ns.prompt(`Would you like a quickstart guide?`);
+    if (helpCheck == true) {
+      ns.tprint(`-------------------------------------------------------------------------------------------------------------`);
+      ns.tprint(`To begin, copy and paste the contents of /utils/alias.txt into the terminal and run.`);
+      ns.tprint(`--------------------------------`);
+      ns.tprint(`Next, use Sire to CRACK a target. Syntax:`);
+      ns.tprint(`sire crack [target]`);
+      ns.tprint(`--------------------------------`);
+      ns.tprint(`Then use Sire to ATTACK your target. Syntax:`);
+      ns.tprint(`sire attack [target]`);
+      ns.tprint(`--------------------------------`);
+      ns.tprint(`Finally, make sure to start up the listener by running the word "list"`);
+      ns.tprint(`--------------------------------`);
+      ns.tprint(`Warn: The listener script will not run alongside the main script with the starting RAM of 8Gb!`);
+      ns.tprint(`Warn: SireNET advises RAM upgrade ASAP to avoid needing to kill and restart the listener constantly!`);
+      ns.tprint(`-------------------------------------------------------------------------------------------------------------`);
+    }
+  }
+}
